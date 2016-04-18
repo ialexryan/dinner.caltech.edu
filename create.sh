3c660df937784a73321224e31790c189f74cd33a
Alex Ryan <alex@alexryan.org>
2016-04-17 20:29:22 -0700

    Uppercase filenames for compatibility with text

--- create.sh
+++ create.sh
@@ -3,9 +3,9 @@
 wget --quiet --timestamping "http://dining.caltech.edu/documents/42-boardmenu.pdf"
 pdftoppm -f 1 -singlefile -png 42-boardmenu.pdf menu
 
-convert menu.png -crop 245x925+307+143 monday.png
-convert menu.png -crop 245x925+552+143 tuesday.png
-convert menu.png -crop 245x925+797+143 wednesday.png
-convert menu.png -crop 245x925+1042+143 thursday.png
-convert menu.png -crop 270x925+1287+143 friday.png
+convert menu.png -crop 245x925+307+143 Monday.png
+convert menu.png -crop 245x925+552+143 Tuesday.png
+convert menu.png -crop 245x925+797+143 Wednesday.png
+convert menu.png -crop 245x925+1042+143 Thursday.png
+convert menu.png -crop 270x925+1287+143 Friday.png
 
