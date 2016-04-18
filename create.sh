0f8ac3dfe1f6d5fba7f84e1d26f8df9183f77b32
Alex Ryan <ialex.ryan@gmail.com>
2016-04-18 13:02:46 -0700

    Further tweaks to cropping values. Going to need a better system for this.

--- create.sh
+++ create.sh
@@ -3,9 +3,9 @@
 wget --quiet --timestamping "http://dining.caltech.edu/documents/42-boardmenu.pdf"
 pdftoppm -f 1 -singlefile -png 42-boardmenu.pdf menu
 
-convert menu.png -crop 245x925+325+143 Monday.png
-convert menu.png -crop 245x925+577+143 Tuesday.png
-convert menu.png -crop 245x925+822+143 Wednesday.png
-convert menu.png -crop 245x925+1067+143 Thursday.png
-convert menu.png -crop 245x925+1312+143 Friday.png
+convert menu.png -crop 245x970+330+143 Monday.png
+convert menu.png -crop 245x970+582+143 Tuesday.png
+convert menu.png -crop 245x970+827+143 Wednesday.png
+convert menu.png -crop 245x970+1072+143 Thursday.png
+convert menu.png -crop 245x970+1317+143 Friday.png
 
