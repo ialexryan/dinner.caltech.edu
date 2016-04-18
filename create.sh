06ec577f0f13d75d4b68173e987692b8016fa711
Alex Ryan <ialex.ryan@gmail.com>
2016-04-18 12:50:53 -0700

    Change hardcoded cropping values.

--- create.sh
+++ create.sh
@@ -3,9 +3,9 @@
 wget --quiet --timestamping "http://dining.caltech.edu/documents/42-boardmenu.pdf"
 pdftoppm -f 1 -singlefile -png 42-boardmenu.pdf menu
 
-convert menu.png -crop 245x925+307+143 Monday.png
-convert menu.png -crop 245x925+552+143 Tuesday.png
-convert menu.png -crop 245x925+797+143 Wednesday.png
-convert menu.png -crop 245x925+1042+143 Thursday.png
-convert menu.png -crop 270x925+1287+143 Friday.png
+convert menu.png -crop 245x925+325+143 Monday.png
+convert menu.png -crop 245x925+577+143 Tuesday.png
+convert menu.png -crop 245x925+822+143 Wednesday.png
+convert menu.png -crop 245x925+1067+143 Thursday.png
+convert menu.png -crop 245x925+1312+143 Friday.png
 
