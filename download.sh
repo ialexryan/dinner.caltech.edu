38144474acc6fc2a3443787bb23a28327f34ce24
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-10-15 15:40:45 -0700

    Fixed regex to be compatible with CDS's antics

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url=http://dining.sites.caltech.edu/$(curl -s http://dining.sites.caltech.edu/students | grep -o "/documents/[0-9]*/Board_Menu_[0-9]*\.[0-9]*\.pdf")
+url=http://dining.sites.caltech.edu/$(curl -s http://dining.sites.caltech.edu/students | grep -o "/documents/[0-9]*/Board[A-Za-z0-9_\.]*\.pdf")
 
 wget -O menu.pdf --quiet --timestamping "$url"
 pdftoppm -f 1 -singlefile -png menu.pdf menu
