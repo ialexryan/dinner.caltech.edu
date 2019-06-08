47ab1af9158184fd08460262ca3aac6e3e525bcb
Nicholas Currault <nicholas.curr+dev@gmail.com>
2019-06-08 05:43:04 -0700

    if CDS gives multiple links, follow the first

--- download.sh
+++ download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-url=http://dining.sites.caltech.edu/$(curl -s http://dining.sites.caltech.edu/students | grep -o "/documents/[0-9]*/Board[A-Za-z0-9_\.]*\.pdf")
+url=http://dining.sites.caltech.edu/$(curl -s http://dining.sites.caltech.edu/students | grep -o "/documents/[0-9]*/Board[A-Za-z0-9_\.]*\.pdf" | head -1)
 
 wget -O menu.pdf --quiet --timestamping "$url"
-
-rm menu-*.png
 pdftoppm -png menu.pdf menu
