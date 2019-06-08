f4d4c784c850410362c17e0ddb2a640def3c3b0c
Nicholas Currault <nicholas.curr+dev@gmail.com>
2019-06-08 05:35:05 -0700

    compatibility with new menu format

--- crop.py
+++ crop.py
@@ -1,66 +1,123 @@
 from PIL import Image
 
-im = Image.open("menu.png")
-pix = im.load()
+ORANGE = (255, 102, 0)
+WHITE = (255, 255, 255)
 
-width, height = im.size
+def parse_pair(im):
+    pix = im.load()
+    width, height = im.size
 
-# Okay, first we're going to find out where the top of the grid is.
-# We're going to do that by choosing a column of pixels about 1/3 of
-# the way in from the left side of the document, and then iterating
-# down that column of pixels until we see something that isn't white.
-top_of_grid = -1
-for i in range(0, height):
-  val = pix[(width/3), i]
-  if val[0] < 255 and val[1] < 255:
-    top_of_grid = i
-    break
-assert(top_of_grid != -1)
-# print "Found the top of the grid at row " + str(top_of_grid)
+    div_line_bottom = -1
+    div_line_left = -1
+    div_line_right = -1
+    div_line_top = -1
 
-# Now we're going to find the bottom of the grid. It's the same idea,
-# but a little trickier because we have to ignore the two rows of
-# "legend" information at the bottom of the page.
-bottom_of_legend = False
-top_of_legend = False
-bottom_of_grid = -1
-for i in reversed(range(0, height)):
-  val = pix[(width/4), i] # one cell in the legend is white at 1/3 over, so that was messing up
-  if not bottom_of_legend:
-    if val[0] < 255 and val[1] < 255:
-      bottom_of_legend = True
-  elif not top_of_legend:
-    if val[0] == 255 and val[1] == 255:
-      top_of_legend = True
-  else:
-    if val[0] < 255 and val[1] < 255:
-      bottom_of_grid = i
-      break
-assert(bottom_of_legend)
-assert(top_of_legend)
-assert(bottom_of_grid != -1)
-# print "Found the bottom of the grid at row " + str(bottom_of_grid)
+    for y in range(height - 1, -1, -1):
+        for x in range((width // 2) - 20, (width // 2) + 20):
+            val = pix[x, y]
+            if div_line_left == -1 and val == ORANGE:
+                div_line_left = x
+                div_line_bottom = y
+            elif div_line_left != -1 and val != ORANGE:
+                div_line_right = x - 1
+                break
+        if div_line_left != -1:
+            break
 
-# Now that we know where the top of the grid is, we're going to iterate
-# over the row of pixels just under it and identify all the column boundaries.
-boundaries = []
-prev_val = pix[0, top_of_grid + 3]
-for i in range(1, width):
-	val = pix[i, top_of_grid + 3]
-	if val != prev_val and prev_val == (0,0,0):
-		boundaries.append(i)
-	prev_val = val
-assert(len(boundaries) == 7)
-assert(width/3 not in boundaries)
-# print "We found column boundaries at " + str(boundaries)
+    div_line_mid = (div_line_left + div_line_right) // 2
 
-Monday = im.crop((boundaries[1], top_of_grid, boundaries[2], bottom_of_grid))
-Tuesday = im.crop((boundaries[2], top_of_grid, boundaries[3], bottom_of_grid))
-Wednesday = im.crop((boundaries[3], top_of_grid, boundaries[4], bottom_of_grid))
-Thursday = im.crop((boundaries[4], top_of_grid, boundaries[5], bottom_of_grid))
-Friday = im.crop((boundaries[5], top_of_grid, boundaries[6], bottom_of_grid))
-Monday.save("Monday.png", "PNG")
-Tuesday.save("Tuesday.png", "PNG")
-Wednesday.save("Wednesday.png", "PNG")
-Thursday.save("Thursday.png", "PNG")
-Friday.save("Friday.png", "PNG")
+    for y in range(div_line_bottom, -1, -1):
+        if pix[div_line_mid, y] != ORANGE:
+            div_line_top = y + 1
+            break
+
+    assert(div_line_bottom != -1)
+    assert(div_line_left != -1)
+    assert(div_line_right != -1)
+    assert(div_line_top != -1)
+
+    left = im.crop((0, div_line_top - 60, div_line_left - 1, div_line_bottom))
+    right = im.crop((div_line_right + 1, div_line_top - 60, width, div_line_bottom))
+    return left, right
+
+def parse_single(im):
+    pix = im.load()
+    width, height = im.size
+
+    left_boundary = -1
+    right_boundary = -1
+    bottom_boundary = -1
+    top_boundary = -1
+
+    for x in range(width - 1):
+        val1 = pix[x, height // 2]
+        val2 = pix[x + 1, height // 2]
+        if val1 != WHITE and val2 == WHITE:
+            left_boundary = x + 1
+            break
+    assert(left_boundary != -1)
+
+    for x in range(width - 1, 0, -1):
+        val1 = pix[x, height // 2]
+        val2 = pix[x - 1, height // 2]
+        if val1 != WHITE and val2 == WHITE:
+            right_boundary = x - 1
+            break
+    assert(right_boundary != -1)
+
+    for y in range(height // 2, height):
+        if pix[left_boundary - 1, y] == WHITE:
+            bottom_boundary = y
+            break
+    assert(bottom_boundary != -1)
+
+    for y in range(height // 2, -1, -1):
+        if pix[left_boundary - 1, y] == WHITE:
+            top_boundary = y
+            break
+    assert(top_boundary != -1)
+
+    return im.crop((left_boundary, top_boundary - 60, right_boundary, bottom_boundary))
+
+def crop_sides(im, margin = 50):
+    width, height = im.size
+    pix = im.load()
+
+    content_left = -1
+    content_right = -1
+
+    for x in range(width):
+        if any(pix[x, y] != WHITE for y in range(height)):
+            content_left = x
+            break
+    assert(content_left != -1)
+
+    for x in range(width - 1, -1, -1):
+        if any(pix[x, y] != WHITE for y in range(height)):
+            content_right = x
+            break
+    assert(content_right != -1)
+
+    margin = min(margin, content_left, width - content_right)
+    return im.crop((content_left - margin, 0, content_right + margin, height))
+
+
+im1 = Image.open("menu-1.png")
+im2 = Image.open("menu-2.png")
+im3 = Image.open("menu-3.png")
+
+mon, tues = parse_pair(im1)
+wed, thurs = parse_pair(im2)
+fri = parse_single(im3)
+
+mon = crop_sides(mon)
+tues = crop_sides(tues)
+wed = crop_sides(wed)
+thurs = crop_sides(thurs)
+fri = crop_sides(fri)
+
+mon.save("Monday.png", "PNG")
+tues.save("Tuesday.png", "PNG")
+wed.save("Wednesday.png", "PNG")
+thurs.save("Thursday.png", "PNG")
+fri.save("Friday.png", "PNG")
--- download.sh
+++ download.sh
@@ -3,4 +3,6 @@
 url=http://dining.sites.caltech.edu/$(curl -s http://dining.sites.caltech.edu/students | grep -o "/documents/[0-9]*/Board[A-Za-z0-9_\.]*\.pdf")
 
 wget -O menu.pdf --quiet --timestamping "$url"
-pdftoppm -f 1 -singlefile -png menu.pdf menu
+
+rm menu-*.png
+pdftoppm -png menu.pdf menu
