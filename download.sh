fac464b76d83686397897702910af6e27ae5c611
Nicholas Currault <nicholas.curr+dev@gmail.com>
2019-11-11 16:12:26 -0800

    more adaptations, CDS moved from /students to /board-program

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url=http://dining.sites.caltech.edu/$(curl -s http://dining.caltech.edu/locations/chandler | grep -o "documents/[0-9]*/Board[A-Za-z0-9_\.]*\.pdf" | head -1)
+url=http://dining.sites.caltech.edu/$(curl -s http://dining.caltech.edu/board-program | grep -o "documents/[0-9]*/Board[A-Za-z0-9_\.]*\.pdf" | head -1)
 
 wget -O menu.pdf --quiet --timestamping "$url"
 pdftoppm -png menu.pdf menu
